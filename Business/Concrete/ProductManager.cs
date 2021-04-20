0de4167ec9bc0c3c825fbc0dac6b0cf254215960
muhammedalitunc1412 <tuncm606@gmail.com>
2021-04-20 10:38:09 +0300

    Template Example

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            if (product.CategoryId==1)
+            {
+                return new ErrorResult("Bu ürün kategorri kabul etmiyor");
+            }
+            else
+            {
+                return new SuccessResult("ürün eklendi");
+            }
+        }
+
+        public IDataResult<List<Product>> GetAll()
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), "Gayet güzel ürünler listelendi");
+        }
+
+        public IDataResult<List<Product>> GetAllByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==categoryId), "Gayet güzel ürünler listelendi");
+        }
+    }
+}
